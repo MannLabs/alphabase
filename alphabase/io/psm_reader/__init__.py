c517ce1f418efe9afc26ad06cf0b505b63c77534
Zeng, Wen-Feng <jalew188@gmail.com>
2022-01-11 23:39:44 +0100

    msfragger_reader

--- alphabase/io/psm_reader/__init__.py
+++ alphabase/io/psm_reader/__init__.py
@@ -5,6 +5,12 @@ import alphabase.io.psm_reader.dia_search_reader
 import alphabase.io.psm_reader.maxquant_reader
 import alphabase.io.psm_reader.msfragger_reader
 import alphabase.io.psm_reader.pfind_reader
+
+try:
+    import alphabase.io.psm_reader.msfragger_reader
+except ImportError:
+    pass
+
 from alphabase.io.psm_reader.psm_reader import (
     psm_reader_provider
 )
