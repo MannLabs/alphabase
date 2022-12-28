ba3271a0348c018c96b5b92fabd9e8fc66dce553
Zeng, Wen-Feng <jalew188@gmail.com>
2022-12-28 10:00:07 +0100

    Bump version: 0.3.1 → 0.4.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ project = 'alphabase'
 copyright = '2022, Mann Labs, MPIB'
 author = 'Mann Labs, MPIB'
 
-release = "0.3.1"
+release = "0.4.0"
 
 # -- General configuration ---------------------------------------------------
 
