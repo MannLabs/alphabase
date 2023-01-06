cc091aed31de716e7e9a2aa5edd17c1141761ccd
Zeng, Wen-Feng <jalew188@gmail.com>
2023-01-06 07:59:09 +0100

    CHORE

--- alphabase/__init__.py
+++ alphabase/__init__.py
@@ -18,8 +18,8 @@ __classifiers__ = [
     # "Development Status :: 1 - Planning",
     # "Development Status :: 2 - Pre-Alpha",
     # "Development Status :: 3 - Alpha",
-    "Development Status :: 4 - Beta",
-    # "Development Status :: 5 - Production/Stable",
+    # "Development Status :: 4 - Beta",
+    "Development Status :: 5 - Production/Stable",
     # "Development Status :: 6 - Mature",
     # "Development Status :: 7 - Inactive"
     "Intended Audience :: Science/Research",
