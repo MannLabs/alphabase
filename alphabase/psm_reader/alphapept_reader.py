9fef33e6355b069226c0d6c98338f8a20b927d10
Zeng, Wen-Feng <jalew188@gmail.com>
2023-01-22 23:12:35 +0100

    alphapept psm: peptide_fdr ->  identifications instead of peptide_fdr

--- alphabase/psm_reader/alphapept_reader.py
+++ alphabase/psm_reader/alphapept_reader.py
@@ -50,15 +50,6 @@ def parse_ap(precursor):
 
     return ''.join(parsed), ';'.join(mods), ';'.join(sites), charge, decoy
 
-def get_x_tandem_score(df: pd.DataFrame) -> np.ndarray:
-    b = df['hits_b'].astype('int').apply(lambda x: np.math.factorial(x)).values
-    y = df['hits_y'].astype('int').apply(lambda x: np.math.factorial(x)).values
-    x_tandem = np.log(b.astype('float')*y.astype('float')*df['fragments_matched_int_sum'].values)
-
-    x_tandem[x_tandem==-np.inf] = 0
-
-    return x_tandem
-
 class AlphaPeptReader(PSMReaderBase):
     def __init__(self,
         *,
@@ -78,10 +69,7 @@ class AlphaPeptReader(PSMReaderBase):
             keep_decoy = keep_decoy,
             **kwargs,
         )
-        if fdr <= 0.1:
-            self.hdf_dataset = 'peptide_fdr'
-        else:
-            self.hdf_dataset = 'second_search'
+        self.hdf_dataset = 'identifications'
 
     def _init_column_mapping(self):
         self.column_mapping = psm_reader_yaml[
@@ -104,9 +92,6 @@ class AlphaPeptReader(PSMReaderBase):
                 df['scan_no'] = df['scan_no'].astype('int')
                 df['raw_idx'] = df['scan_no']-1 # if thermo, use scan-1 as spec_idx
             df['charge'] = df['charge'].astype(int)
-
-            if 'score' not in df.columns:
-                df['score'] = get_x_tandem_score(df)
         return df
     
     def _load_modifications(self, df: pd.DataFrame):
