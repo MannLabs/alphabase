fc61f948d9837a9d7f12a246810a56a09cb2860e
jalew188 <jalew188@gmail.com>
2023-05-06 16:04:06 +0200

    dtype()->dtype().type to get np.floatx

--- alphabase/constants/_const.py
+++ alphabase/constants/_const.py
@@ -15,5 +15,5 @@ common_const_dict:dict = load_yaml(
 # Only applied in peak and fragment dataframes to save RAM. 
 # Using float32 still keeps 0.1 ppm precision in any value range.
 # Default float dtype is "float64" for value calculation and other senarios.
-PEAK_MZ_DTYPE = np.dtype(common_const_dict["PEAK_MZ_DTYPE"])
-PEAK_INTENSITY_DTYPE = np.dtype(common_const_dict["PEAK_INTENSITY_DTYPE"])
+PEAK_MZ_DTYPE = np.dtype(common_const_dict["PEAK_MZ_DTYPE"]).type
+PEAK_INTENSITY_DTYPE = np.dtype(common_const_dict["PEAK_INTENSITY_DTYPE"]).type
