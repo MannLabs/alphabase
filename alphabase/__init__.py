3a7bc0b4cb630915075e148f679f7826962ab2f2
jalew188 <jalew188@gmail.com>
2024-03-15 20:44:11 +0100

    Bump version: 1.2.1 → 1.2.2

--- alphabase/__init__.py
+++ alphabase/__init__.py
@@ -2,7 +2,7 @@
 
 
 __project__ = "alphabase"
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 __license__ = "Apache"
 __description__ = "An infrastructure Python package of the AlphaX ecosystem"
 __author__ = "Mann Labs"
