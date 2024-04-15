ef96765cb30fe8492f877a95a3848667260e1537
Sophia Mädler <15019107+sophiamaedler@users.noreply.github.com>
2024-04-15 20:38:21 +0200

    add function to explicitly initialize a HDF5 file prepared for using as a tempmmap array

--- alphabase/io/tempmmap.py
+++ alphabase/io/tempmmap.py
@@ -92,7 +92,59 @@ def array(shape: tuple, dtype: np.dtype) -> np.ndarray:
             offset=offset
         ).reshape(shape)
 
-def mmap_array(hdf_file: str) -> np.ndarray:
+def create_empty_mmap(shape: tuple, dtype: np.dtype, path: str = None, overwrite = False):
+    """Initialize a new HDF5 file compatible with mmap. Returns the path to the initialized file.
+    File can be mapped using the mmap_array_from_path function.
+    
+    Parameters
+    ----------
+    shape : tuple
+        A tuple with the shape of the array.
+    dtype : type
+        The np.dtype of the array.
+    path : str, optional
+        The path to the file that should be created.
+        Defaults to None.
+        If None a random file name will be generated.
+    overwrite : bool , optional
+        If True the file will be overwritten if it already exists.
+        Defaults to False.
+
+    Returns
+    -------
+    str
+        path to the newly created file.
+    """
+    
+    #if path does not exist generate a random file name in the TEMP directory
+    if path is None:
+        temp_file_name = os.path.join(
+            TEMP_DIR_NAME,
+            f"temp_mmap_{np.random.randint(2**63)}.hdf"
+        )
+    else:
+        #check that if overwrite is false the file does not already exist
+        if not overwrite:
+            if os.path.exists(path):
+                raise ValueError("The file already exists. Set overwrite to True to overwrite the file or choose a different name.")
+        if not os.path.basename.endswith('.hdf'):
+            raise ValueError("The chosen file name needs to end with .hdf")
+        if os.path.isdir(os.path.commonpath(path)):
+            temp_file_name = path
+        else:
+            raise ValueError("The directory in which the file should be created does not exist.")
+        
+    with h5py.File(temp_file_name, "w") as hdf_file:
+        array = hdf_file.create_dataset(
+            "array",
+            shape=shape,
+            dtype=dtype
+        )
+        array[0] = 0
+
+    return temp_file_name
+
+def mmap_array_from_path(hdf_file: str) -> np.ndarray:
     """reconnect to an exisiting HDF5 file to generate a writable temporary mmapped array.
 
     Parameters
@@ -127,6 +179,7 @@ def mmap_array(hdf_file: str) -> np.ndarray:
             count=np.prod(shape),
             offset=offset
         ).reshape(shape)
+
 
 def zeros(shape: tuple, dtype: np.dtype) -> np.ndarray:
     """Create a writable temporary mmapped array filled with zeros.
