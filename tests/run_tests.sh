3147bc79036d6d6c57611a98b406057bceae2d82
jalew188 <jalew188@gmail.com>
2024-05-16 10:31:40 +0200

    FIX test_isotope_mp.ipynb; move nbdev_nbs to nbs_tests; move nbs to scripts

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,10 +1,11 @@
-INCLUDED_NBS=$(find ./nbdev_nbs -name "*.ipynb")
+# TODO make tutorial_dev_spectral_libraries.ipynb work
+INCLUDED_NBS=$(find ../docs/nbs -name "*.ipynb" | grep -v tutorial_dev_spectral_libraries.ipynb)
 python -m pytest --nbmake $(echo $INCLUDED_NBS)
 
+# TODO make test_isotope_mp.ipynb work
+# Note: multiprocessing in ipynb sometimes suspended on some versions of Windows, ignore the
+# corresponding notebook(s) if this occurs again
+# INCLUDED_NBS=$(find ../nbs_tests -name "*.ipynb" | grep -v test_isotope_mp.ipynb)
 
-INCLUDED_NBS=$(find ./docs/nbs -name "*.ipynb" | grep -v tutorial_dev_spectral_libraries.ipynb)
-python -m pytest --nbmake $(echo $INCLUDED_NBS)
-
-
-INCLUDED_NBS=$(find ./nbs_tests -name "*.ipynb" | grep -v test_isotope_mp.ipynb)
+INCLUDED_NBS=$(find ../nbs_tests -name "*.ipynb")
 python -m pytest --nbmake $(echo $INCLUDED_NBS)
