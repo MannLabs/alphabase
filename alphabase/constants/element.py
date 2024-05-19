e99fb0b9b2c9beb81986e0fadf6dd8f9398e786b
jalew188 <jalew188@gmail.com>
2024-05-19 11:06:15 +0200

    ADD element.py with DeprecationWarning

---
+++ alphabase/constants/element.py
@@ -0,0 +1,7 @@
+from alphabase.constants.atom import *
+
+import warnings
+warnings.warn(
+    "The module `alphabase.constants.element` is deprecated, it will be removed in alphabase>=1.3.0",
+    DeprecationWarning
+)
