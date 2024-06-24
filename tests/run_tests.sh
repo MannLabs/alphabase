0e0774de7f9c092490a14dff1910fa2738ac18fa
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-06-24 17:37:01 +0200

    run only one notebook in tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,6 @@
 # TODO make tutorial_dev_spectral_libraries.ipynb work
-INCLUDED_NBS=$(find ../docs/nbs -name "*.ipynb" | grep -v tutorial_dev_spectral_libraries.ipynb)
-python -m pytest --nbmake $(echo $INCLUDED_NBS)
+#INCLUDED_NBS=$(find ../docs/nbs -name "*.ipynb" | grep -v tutorial_dev_spectral_libraries.ipynb)
+#python -m pytest --nbmake $(echo $INCLUDED_NBS)
 
 # TODO make test_isotope_mp.ipynb work
 # Note: multiprocessing in ipynb sometimes suspended on some versions of Windows, ignore the
