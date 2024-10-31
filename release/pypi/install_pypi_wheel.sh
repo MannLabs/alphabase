0177c090c6820d8b20562a6a42ca148bf56ff468
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-10-31 07:26:14 +0100

    delete release folder

--- release/pypi/install_pypi_wheel.sh
+++
@@ -1,5 +0,0 @@
-conda create -n alphabase_pip_test python=3.9 -y
-conda activate alphabase_pip_test
-pip install "alphabase[stable]"
-alphabase
-conda deactivate
--- release/pypi/install_test_pypi_wheel.sh
+++
@@ -1,5 +0,0 @@
-conda create -n alphabase_pip_test python=3.9 -y
-conda activate alphabase_pip_test
-pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple "alphabase[stable]"
-alphabase
-conda deactivate
