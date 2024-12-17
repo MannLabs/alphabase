a541eb8b2e3f32ed9b01009843040188b94b9ff1
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-12-17 09:33:27 +0100

    add test skeleton

--- tests/unit/io/test_tempmmap.py
+++ tests/unit/io/test_tempmmap.py
@@ -5,26 +5,53 @@ This module has a nontrivial lifecycle (top-level code), therefore the test setu
 """
 
 import sys
+import tempfile
 
 import numpy as np
 
 
 def setup_function(function):
-    """Import the module before evert"""
+    """Import the module before every test."""
+    import alphabase.io.tempmmap  # noqa
 
 
 def teardown_function(function):
-    # simulating @atexit.register
-    sys.modules["alphabase.io.tempmmap"].clear()
+    """Simulate `atexit.register` and delete the module before every test."""
+    tempmmap = sys.modules["alphabase.io.tempmmap"]
+    tempmmap.clear()  # simulating @atexit.register
+
+    # # later:
+    # assert tempmmap._TEMP_DIR is None
+    # assert tempmmap.TEMP_DIR_NAME is None
+
     del sys.modules["alphabase.io.tempmmap"]
 
 
-def test_create_array_with_valid_shape_and_dtype():
+def test_create_array():
     """Test creating and accessing an array."""
     tempmmap = sys.modules["alphabase.io.tempmmap"]
-    arr = tempmmap.array((10, 10), np.float32)
-    arr[1, 1] = 1
 
-    assert arr.shape == (10, 10)
+    # when
+    arr = tempmmap.array((5, 5), np.float32)
+    assert arr[1, 1] == 0
+    assert arr.shape == (5, 5)
     assert arr.dtype == np.float32
+
+    # test rw access to array
+    arr[1, 1] = 1
     assert arr[1, 1] == 1
+
+
+def test_create_array_with_custom_temp_dir():
+    tempmmap = sys.modules["alphabase.io.tempmmap"]
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        # when
+        arr = tempmmap.array((5, 5), np.int32, tmp_dir_abs_path=temp_dir)
+        assert arr.shape == (5, 5)
+        assert arr.dtype == np.int32
+        assert arr[1, 1] == 0
+
+        # test rw access to array
+        arr[1, 1] = 1
+        assert arr[1, 1] == 1
