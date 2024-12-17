97dca5f92db1d70fc4b965ce84425a624f3b394c
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-12-17 09:48:53 +0100

    add test_mmap_array_from_path

--- tests/unit/io/test_tempmmap.py
+++ tests/unit/io/test_tempmmap.py
@@ -4,6 +4,7 @@ This module has a nontrivial lifecycle (top-level code), therefore the test setu
 (explicit handling of module (un)loading in setup_function()/teardown_function())
 """
 
+import os
 import sys
 import tempfile
 
@@ -51,16 +52,39 @@ def test_create_array_with_custom_temp_dir():
         # when
         arr = tempmmap.array((5, 5), np.int32, tmp_dir_abs_path=temp_dir)
 
-    assert arr.shape == (5, 5)
-    assert arr.dtype == np.int32
-    assert arr[1, 1] == 0
+        assert arr.shape == (5, 5)
+        assert arr.dtype == np.int32
+        assert arr[1, 1] == 0
 
-    # test rw access to array
-    arr[1, 1] = 1
-    assert arr[1, 1] == 1
+        # test rw access to array
+        arr[1, 1] = 1
+        assert arr[1, 1] == 1
 
     assert tempmmap._TEMP_DIR is not None
     assert temp_dir == tempmmap.TEMP_DIR_NAME
+
+
+def test_mmap_array_from_path():
+    tempmmap = sys.modules["alphabase.io.tempmmap"]
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        # first create an array ..
+        arr = tempmmap.array((5, 5), np.int32, tmp_dir_abs_path=temp_dir)
+        arr[1, 1] = 1
+
+        # then reconnect to it
+        temp_dir = tempmmap.TEMP_DIR_NAME
+        # this is a bit hacky but given all the randomness in the temp file name, should be ok:
+        temp_file_name = os.listdir(temp_dir)[0]
+
+        reconnected_arr = tempmmap.mmap_array_from_path(f"{temp_dir}/{temp_file_name}")
+        assert reconnected_arr[1, 1] == 1
+
+        reconnected_arr[1, 1] = 2
+        assert reconnected_arr[1, 1] == 2
+
+        arr[1, 1] = 3
+        assert reconnected_arr[1, 1] == 3
 
 
 def test_create_zeros():
