18dd8b3e8a91ee379f69a7a4996ef06672d7b9fe
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-01-16 12:03:29 +0100

    remove development-stable

--- misc/stable_pip_install.sh
+++ misc/stable_pip_install.sh
@@ -1,5 +1,5 @@
 conda create -n alphabase python=3.9 -y
 conda activate alphabase
-pip install -e '../.[stable,development-stable]' -U
+pip install -e '../.[stable,development]' -U
 alphabase
 conda deactivate
