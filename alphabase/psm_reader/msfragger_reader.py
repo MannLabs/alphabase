33b5b1aedac39e1cdde6a1bf40f96b3f3840c161
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-02-18 14:52:51 +0100

    fix formatting

--- alphabase/psm_reader/msfragger_reader.py
+++ alphabase/psm_reader/msfragger_reader.py
@@ -1,11 +1,11 @@
 """MSFragger reader."""
 
+import logging
 import warnings
 from typing import List, Optional, Tuple
 
 import numpy as np
 import pandas as pd
-import logging
 
 try:
     from pyteomics import pepxml
