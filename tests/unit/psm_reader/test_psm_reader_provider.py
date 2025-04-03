c8523e65b16990bddbd2dcc4fb99d91ce60f8a8e
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-04-03 15:42:15 +0200

    Add test documentation

--- tests/unit/psm_reader/test_psm_reader_provider.py
+++ tests/unit/psm_reader/test_psm_reader_provider.py
@@ -19,10 +19,12 @@ from alphabase.psm_reader import PSMReaderBase, psm_reader_provider
     ],
 )
 def test_psm_reader_provider(reader_type):
+    """Test whether the PSM reader provider returns a PSM Reader"""
     reader = psm_reader_provider.get_reader(reader_type=reader_type)
     assert isinstance(reader, PSMReaderBase)
 
 
 def test_psm_reader_provider_error():
+    """Test whether the PSM reader provider raises an error in case of passing an invalid reader name"""
     with pytest.raises(KeyError):
         psm_reader_provider.get_reader(reader_type="this_is_not_a_valid_reader")
