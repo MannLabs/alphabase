59bba36c4c79389359edc5b64d83142494669746
null <82171591+mschwoer@users.noreply.github.com>
2025-04-10 09:59:14 +0000

    Bump version: 1.6.2-dev0 → 1.6.2

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ project = "alphabase"
 copyright = "2022, Mann Labs, MPIB"
 author = "Mann Labs, MPIB"
 
-release = "1.6.2-dev0"
+release = "1.6.2"
 
 # -- General configuration ---------------------------------------------------
 
