d928ffec8417b64b4ba63861cbf099725a39444a
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-04-10 11:33:09 +0200

    add missing yaml dependency and tests

--- alphabase/yaml_utils.py
+++ alphabase/yaml_utils.py
@@ -3,8 +3,7 @@ import yaml
 
 def load_yaml(filename) -> dict:
     with open(filename) as f:
-        settings = yaml.load(f, Loader=yaml.FullLoader)
-    return settings
+        return yaml.load(f, Loader=yaml.FullLoader)
 
 
 def save_yaml(filename, settings):
