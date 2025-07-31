f460ca1d3c278cd0e9b76a57e1a0d2a0528d1c16
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-07-31 20:26:51 +0200

    [Feature] Add additional keys

--- alphabase/pg_reader/keys.py
+++ alphabase/pg_reader/keys.py
@@ -14,9 +14,20 @@ class PGCols(metaclass=ConstantsClass):
     UNIPROT_IDS
         Uniprot IDs of all proteins in the respective protein group.
         Individual entries are separated by a semicolon in the unified output.
+    GENES
+        Gene names encoding the proteins in the respective protein group. Uses HGNC names for humans.
+        Individual entries are separated by a semicolon in the unified output.
+    PROTEIN_CANDIDATES
+        Uniprot IDs of all proteins matching a specific precursor before protein inference.
+        Individual entries are separated by a semicolon in the unified output.
+    DESCRIPTION
+        Long text description of one or more proteins in the respective protein group.
 
     """
 
     # Minimal columns
     PROTEINS = "proteins"
     UNIPROT_IDS = "uniprot_ids"
+    GENES = "genes"
+    PROTEIN_CANDIDATES = "protein_candidates"
+    DESCRIPTION = "description"
