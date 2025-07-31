79187f926e0fd1f16aa31b14b80ec0868a0991b6
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-07-31 20:18:28 +0200

    [Doc] Add description of PG attributes to streamline usage of attributes

--- alphabase/pg_reader/keys.py
+++ alphabase/pg_reader/keys.py
@@ -4,7 +4,19 @@ from alphabase.psm_reader.keys import ConstantsClass
 
 
 class PGCols(metaclass=ConstantsClass):
-    """Standardized keys for PG readers."""
+    """Standardized keys for PG readers.
+
+    Attributes
+    ----------
+    PROTEINS
+        Uniprot names of proteins in the respective protein group.
+        Individual entries are separated by a semicolon in the unified output.
+    UNIPROT_IDS
+        Uniprot IDs of all proteins in the respective protein group.
+        Individual entries are separated by a semicolon in the unified output.
+
+    """
 
     # Minimal columns
     PROTEINS = "proteins"
+    UNIPROT_IDS = "uniprot_ids"
