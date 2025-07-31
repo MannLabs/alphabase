0888a77737b7cb153d59b09a2d040eb7712c4fa3
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-07-31 23:42:43 +0200

    [Feature] Add keys for alphapept reader

--- alphabase/pg_reader/keys.py
+++ alphabase/pg_reader/keys.py
@@ -14,6 +14,9 @@ class PGCols(metaclass=ConstantsClass):
     UNIPROT_IDS
         Uniprot IDs of all proteins in the respective protein group.
         Individual entries are separated by a semicolon in the unified output.
+    ENSEMBL_IDS
+        ENSEMBL IDs of proteins in the respective protein group.
+        Individual entries are separated by a semicolon in the unified output.
     GENES
         Gene names encoding the proteins in the respective protein group. Uses HGNC names for humans.
         Individual entries are separated by a semicolon in the unified output.
@@ -22,12 +25,21 @@ class PGCols(metaclass=ConstantsClass):
         Individual entries are separated by a semicolon in the unified output.
     DESCRIPTION
         Long text description of one or more proteins in the respective protein group.
+    SOURCE_DB
+        Source databases for a specific feature, e.g. Uniprot, Swissprot, Ensembl.
+        Individual entries are separated by a semicolon in the unified output.
+    DECOY_INDICATOR
+        Boolean that indicates that the respective feature is maked as a decoy by the respective
+        search engine.
 
     """
 
     # Minimal columns
     PROTEINS = "proteins"
     UNIPROT_IDS = "uniprot_ids"
+    ENSEMBL_IDS = "ensembl_ids"
     GENES = "genes"
     PROTEIN_CANDIDATES = "protein_candidates"
     DESCRIPTION = "description"
+    SOURCE_DB = "source_db"
+    DECOY_INDICATOR = "is_decoy"
