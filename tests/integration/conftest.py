b55e46f21de7d691a2c543070e392000df59acfb
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-07-31 20:40:24 +0200

    [Tests] Add integration tests for alphaDIA

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -14,3 +14,12 @@ def example_alphadia_tsv(tmp_path) -> Path:
 
     download_path = DataShareDownloader(url=URL, output_dir=tmp_path).download()
     return download_path
+
+
+@pytest.fixture(scope="function")
+def example_diann_tsv(tmp_path) -> Path:
+    """Get and parse real alphadia PG report matrix."""
+    URL = "https://datashare.biochem.mpg.de/s/R7GYhwArBO2NS9J"
+
+    download_path = DataShareDownloader(url=URL, output_dir=tmp_path).download()
+    return download_path
--- tests/integration/test_pg_readers.py
+++ tests/integration/test_pg_readers.py
@@ -1,6 +1,6 @@
 """Integration tests for protein group reader."""
 
-from alphabase.pg_reader import AlphaDiaPGReader
+from alphabase.pg_reader import AlphaDiaPGReader, DiannPGReader
 from alphabase.pg_reader.keys import PGCols
 
 
@@ -12,3 +12,19 @@ class TestAlphaDiaPGReaderImportIntegration:
 
         assert result_df.shape == (9364, 6)
         assert result_df.index.name == PGCols.UNIPROT_IDS
+
+
+class TestDiannPGReaderImportIntegration:
+    def test_import_real_file(self, example_diann_tsv: str) -> None:
+        reader = DiannPGReader()
+
+        result_df = reader.import_file(example_diann_tsv)
+
+        assert result_df.shape == (10, 20)
+        assert result_df.index.names == [
+            PGCols.PROTEINS,
+            PGCols.UNIPROT_IDS,
+            PGCols.GENES,
+            PGCols.PROTEIN_CANDIDATES,
+            PGCols.DESCRIPTION,
+        ]
