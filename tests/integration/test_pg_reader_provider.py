5441fd84c60f2516f4c85b997999c8944ca2382d
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-07-31 20:40:24 +0200

    [Tests] Add integration tests for alphaDIA

--- tests/integration/test_pg_reader_provider.py
+++ tests/integration/test_pg_reader_provider.py
@@ -1,6 +1,6 @@
 """Integration tests for protein group reader provider."""
 
-from alphabase.pg_reader import AlphaDiaPGReader, pg_reader_provider
+from alphabase.pg_reader import AlphaDiaPGReader, DiannPGReader, pg_reader_provider
 from alphabase.pg_reader.keys import PGCols
 
 
@@ -19,3 +19,26 @@ class TestAlphaDiaPGReaderProvider:
 
         assert result_df.shape == (9364, 6)
         assert result_df.index.name == PGCols.UNIPROT_IDS
+
+
+class TestDiannPGReaderProvider:
+    def test_reader_provider(self) -> None:
+        """Test whether reader provider initializes alphadia PG reader correctly."""
+        reader = pg_reader_provider.get_reader("diann")
+
+        assert isinstance(reader, DiannPGReader)
+
+    def test_reader_provider_import(self, example_diann_tsv: str) -> None:
+        """Test if diann protein group report import works via `pg_reader_provider`"""
+        reader = pg_reader_provider.get_reader("diann")
+
+        result_df = reader.import_file(example_diann_tsv)
+
+        assert result_df.shape == (10, 20)
+        assert result_df.index.names == [
+            PGCols.PROTEINS,
+            PGCols.UNIPROT_IDS,
+            PGCols.GENES,
+            PGCols.PROTEIN_CANDIDATES,
+            PGCols.DESCRIPTION,
+        ]
