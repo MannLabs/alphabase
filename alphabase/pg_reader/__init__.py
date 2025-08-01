d8415b948ff25bb19127d9c3c100ab9e31dc4135
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-08-01 08:59:08 +0200

    [API] Expose alphapept reader

--- alphabase/pg_reader/__init__.py
+++ alphabase/pg_reader/__init__.py
@@ -1,5 +1,11 @@
 from .alphadia_pg_reader import AlphaDiaPGReader
+from .alphapept_pg_reader import AlphaPeptPGReader
 from .diann_pg_reader import DiannPGReader
 from .pg_reader import pg_reader_provider
 
-__all__ = ["pg_reader_provider", "AlphaDiaPGReader", "DiannPGReader"]
+__all__ = [
+    "pg_reader_provider",
+    "AlphaDiaPGReader",
+    "DiannPGReader",
+    "AlphaPeptPGReader",
+]
