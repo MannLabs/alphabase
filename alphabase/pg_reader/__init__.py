7c1de7da6fb1e79be1b7fe196b979624160da9cd
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-08-11 18:36:03 +0200

    [API] Expose MaxQuantPGReader to users

--- alphabase/pg_reader/__init__.py
+++ alphabase/pg_reader/__init__.py
@@ -1,6 +1,7 @@
 from .alphadia_pg_reader import AlphaDiaPGReader
 from .alphapept_pg_reader import AlphaPeptPGReader
 from .diann_pg_reader import DiannPGReader
+from .maxquant_pg_reader import MaxQuantPGReader
 from .pg_reader import pg_reader_provider
 
 __all__ = [
@@ -8,4 +9,5 @@ __all__ = [
     "AlphaDiaPGReader",
     "DiannPGReader",
     "AlphaPeptPGReader",
+    "MaxQuantPGReader",
 ]
