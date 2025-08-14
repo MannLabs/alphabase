fbd05a04ecce9d55e265585215b920577dbf8a14
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-08-14 09:47:26 +0200

    [Test] Test full dataframe equality against reference dataframes

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -2,24 +2,39 @@
 
 from pathlib import Path
 
+import pandas as pd
 import pytest
 
 from alphabase.tools.data_downloader import DataShareDownloader
 
 
 @pytest.fixture(scope="function")
-def example_alphadia_tsv(tmp_path) -> Path:
+def example_alphadia_tsv(tmp_path) -> tuple[Path, pd.DataFrame]:
     """Get and parse real alphadia PG report matrix."""
     URL = "https://datashare.biochem.mpg.de/s/cN1tmElfgKOe1cW"
+    REF_URL = "https://datashare.biochem.mpg.de/s/vtgitlrJNeaWl9U"
 
     download_path = DataShareDownloader(url=URL, output_dir=tmp_path).download()
-    return download_path
+    reference_download_path = DataShareDownloader(
+        url=REF_URL, output_dir=tmp_path
+    ).download()
+
+    reference = pd.read_parquet(reference_download_path)
+
+    return download_path, reference
 
 
 @pytest.fixture(scope="function")
-def example_diann_tsv(tmp_path) -> Path:
+def example_diann_tsv(tmp_path) -> tuple[Path, pd.DataFrame]:
     """Get and parse real DIANN PG report matrix."""
     URL = "https://datashare.biochem.mpg.de/s/R7GYhwArBO2NS9J"
+    REF_URL = "https://datashare.biochem.mpg.de/s/g5rsaGeGkbyKNam"  # Ground truth
 
     download_path = DataShareDownloader(url=URL, output_dir=tmp_path).download()
-    return download_path
+    reference_download_path = DataShareDownloader(
+        url=REF_URL, output_dir=tmp_path
+    ).download()
+
+    reference = pd.read_parquet(reference_download_path)
+
+    return (download_path, reference)
--- tests/integration/test_pg_readers.py
+++ tests/integration/test_pg_readers.py
@@ -1,32 +1,27 @@
 """Integration tests for protein group reader."""
 
+import pandas as pd
+
 from alphabase.pg_reader import AlphaDiaPGReader, DiannPGReader
-from alphabase.pg_reader.keys import PGCols
 
 
 class TestAlphaDiaPGReaderImportIntegration:
     def test_import_real_file(self, example_alphadia_tsv: str) -> None:
         """Test import of real AlphaDIA file"""
+        file_path, reference = example_alphadia_tsv
         reader = AlphaDiaPGReader()
 
-        result_df = reader.import_file(example_alphadia_tsv)
+        result_df = reader.import_file(file_path)
 
-        assert result_df.shape == (9364, 6)
-        assert result_df.index.name == PGCols.UNIPROT_IDS
+        pd.testing.assert_frame_equal(result_df, reference)
 
 
 class TestDiannPGReaderImportIntegration:
     def test_import_real_file(self, example_diann_tsv: str) -> None:
         """Test import of real DIANN file"""
+        file_path, reference = example_diann_tsv
         reader = DiannPGReader()
 
-        result_df = reader.import_file(example_diann_tsv)
+        result_df = reader.import_file(file_path)
 
-        assert result_df.shape == (10, 20)
-        assert result_df.index.names == [
-            PGCols.PROTEINS,
-            PGCols.UNIPROT_IDS,
-            PGCols.GENES,
-            PGCols.PROTEIN_CANDIDATES,
-            PGCols.DESCRIPTION,
-        ]
+        pd.testing.assert_frame_equal(result_df, reference)
