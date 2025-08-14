a5a5a4609ce673c20e8fe34de325222a62453632
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-08-14 10:08:43 +0200

    [Tests] Remove redundant integration tests

--- tests/integration/test_pg_reader_provider.py
+++ tests/integration/test_pg_reader_provider.py
@@ -1,7 +1,6 @@
 """Integration tests for protein group reader provider."""
 
 from alphabase.pg_reader import AlphaDiaPGReader, DiannPGReader, pg_reader_provider
-from alphabase.pg_reader.keys import PGCols
 
 
 class TestAlphaDiaPGReaderProvider:
@@ -11,15 +10,6 @@ class TestAlphaDiaPGReaderProvider:
 
         assert isinstance(reader, AlphaDiaPGReader)
 
-    def test_reader_provider_import(self, example_alphadia_tsv: str) -> None:
-        """Test if import works via `pg_reader_provider`"""
-        reader = pg_reader_provider.get_reader("alphadia")
-
-        result_df = reader.import_file(example_alphadia_tsv)
-
-        assert result_df.shape == (9364, 6)
-        assert result_df.index.name == PGCols.UNIPROT_IDS
-
 
 class TestDiannPGReaderProvider:
     def test_reader_provider(self) -> None:
@@ -27,18 +17,3 @@ class TestDiannPGReaderProvider:
         reader = pg_reader_provider.get_reader("diann")
 
         assert isinstance(reader, DiannPGReader)
-
-    def test_reader_provider_import(self, example_diann_tsv: str) -> None:
-        """Test if DIANN protein group report import works via `pg_reader_provider`"""
-        reader = pg_reader_provider.get_reader("diann")
-
-        result_df = reader.import_file(example_diann_tsv)
-
-        assert result_df.shape == (10, 20)
-        assert result_df.index.names == [
-            PGCols.PROTEINS,
-            PGCols.UNIPROT_IDS,
-            PGCols.GENES,
-            PGCols.PROTEIN_CANDIDATES,
-            PGCols.DESCRIPTION,
-        ]
