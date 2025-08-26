b04b0feebe57bea3808076a2e41281636138e55c
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-08-26 13:38:25 +0200

    [Doc] Add reference to issue

--- misc/pip_install.sh
+++ misc/pip_install.sh
@@ -19,6 +19,7 @@ else
 fi
 
 # pytables has known issues on MacOS for pg-readers - install from conda
+# https://github.com/PyTables/PyTables/issues/219#issuecomment-24117053
 if [ "$OS" = "macOS-latest" ]; then
   conda install -n $ENV_NAME -c conda-forge pytables -y
 elif ["$OS" = "macos-latest-xlarge"]; then
