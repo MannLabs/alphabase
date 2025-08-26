ac93b8afcfc736ef520e26ddc3018cc81df405db
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-08-26 16:01:47 +0200

    [Test] Skip hdf tests if pytables is not available

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,5 +1,6 @@
 """Shared logic for integration tests."""
 
+import importlib.util
 import os
 from pathlib import Path
 
@@ -7,6 +8,8 @@ import pandas as pd
 import pytest
 
 from alphabase.tools.data_downloader import DataShareDownloader
+
+TABLES_PACKAGE_AVAILABLE = importlib.util.find_spec("tables") is not None
 
 
 def get_remote_data_with_ref(
@@ -132,6 +135,10 @@ sp|P35221|CTNA1_HUMAN,225968334.02204236,234103031.64081344,221399683.34428945,2
     return file_path, reference
 
 
+@pytest.mark.skipif(
+    not TABLES_PACKAGE_AVAILABLE,
+    reason="pytables package not installed. Install with `pip install alphabase[hdf]`",
+)
 @pytest.fixture(scope="function")
 def example_alphapept_hdf(tmp_path) -> tuple[Path, pd.DataFrame]:
     """Get and parse real alphapept protein group report matrix."""
