09f19ccddba91ff75686d86473f29d94b9759a57
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-08-26 13:37:09 +0200

    [Fix] MacOS-specific installation of pytables via conda

--- misc/pip_install.sh
+++ misc/pip_install.sh
@@ -8,6 +8,7 @@ set -e -u
 INSTALL_TYPE=$1 # stable, loose, etc..
 ENV_NAME=${2:-alphabase}
 PYTHON_VERSION=${3:-3.9}
+OS=${4:-nan}
 
 conda create -n $ENV_NAME python=$PYTHON_VERSION -y
 
@@ -15,6 +16,13 @@ if [ "$INSTALL_TYPE" = "loose" ]; then
   INSTALL_STRING=""
 else
   INSTALL_STRING="[${INSTALL_TYPE}]"
+fi
+
+# pytables has known issues on MacOS for pg-readers - install from conda
+if [ "$OS" = "macOS-latest" ]; then
+  conda install -n $ENV_NAME -c conda-forge pytables -y
+elif ["$OS" = "macos-latest-xlarge"]; then
+  conda install -n $ENV_NAME -c conda-forge pytables -y
 fi
 
 # print pip environment for reproducibility
