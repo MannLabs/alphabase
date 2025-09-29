31815f88c1fada3a91cef0afec19ef114d7dd77d
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-09-29 18:24:50 +0200

    Bump version: 1.7.1 → 1.7.2. Manual bump

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ project = "alphabase"
 copyright = "2022, Mann Labs, MPIB"
 author = "Mann Labs, MPIB"
 
-release = "1.7.1"
+release = "1.7.2"
 
 # -- General configuration ---------------------------------------------------
 
