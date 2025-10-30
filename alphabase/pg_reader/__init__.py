2bd6b41cb369b5312e6fd4fb1433f1952d23ee61
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-10-30 17:24:35 +0100

    [API] Expose directLFQ reader to users

--- alphabase/pg_reader/__init__.py
+++ alphabase/pg_reader/__init__.py
@@ -1,6 +1,7 @@
 from .alphadia_pg_reader import AlphaDiaPGReader
 from .alphapept_pg_reader import AlphaPeptPGReader
 from .diann_pg_reader import DiannPGReader
+from .directlfq_pg_reader import DirectLFQReader
 from .fragpipe_pg_reader import FragPipePGReader
 from .maxquant_pg_reader import MaxQuantPGReader
 from .mztab_pg_reader import MZTabPGReader
@@ -10,6 +11,7 @@ from .spectronaut_reader import SpectronautPGReader
 __all__ = [
     "pg_reader_provider",
     "AlphaDiaPGReader",
+    "DirectLFQReader",
     "DiannPGReader",
     "AlphaPeptPGReader",
     "MaxQuantPGReader",
