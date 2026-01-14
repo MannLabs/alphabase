b00e79b4a2b586f5ff96e222f43b07755b608ef2
Lucas Diedrich <86159938+lucas-diedrich@users.noreply.github.com>
2026-01-14 14:57:10 +0000

    Bump version: 1.8.0-dev0 → 1.8.0

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ project = "alphabase"
 copyright = "2022, Mann Labs, MPIB"
 author = "Mann Labs, MPIB"
 
-release = "1.8.0-dev0"
+release = "1.8.0"
 
 # -- General configuration ---------------------------------------------------
 
