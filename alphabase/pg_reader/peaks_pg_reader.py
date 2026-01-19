6cc9901e4405eb482390dd8c2cce2cbf9ab94e3e
Vincenth Brennsteiner <brennsteiner@biochem.mpg.de>
2026-01-19 18:11:02 +0100

    working pg and peptide reader for peaks

--- alphabase/pg_reader/peaks_pg_reader.py
+++ alphabase/pg_reader/peaks_pg_reader.py
@@ -9,29 +9,89 @@ from .pg_reader import PGReaderBase, pg_reader_provider
 
 PROTEIN_GENE_SEPARATOR = "|"
 ACCESSION_COLUMN = "Accession"
+EXPECTED_PAIR_LENGTH = 2
+
+
+def _assemble_pg_gene_groups(
+    accession: str,
+    id_separator: str = PROTEIN_GENE_SEPARATOR,
+    group_separator: str = ":",
+) -> tuple[str, str]:
+    """Assemble protein and gene groups from PEAKS accession string.
+
+    Parameters
+    ----------
+    accession
+        Accession string containing protein|gene pairs separated by colons.
+        Format: "protein1|gene1:protein2|gene2:protein3|gene3"
+    id_separator
+        Character separating protein from gene within each pair (default "|").
+    group_separator
+        Character separating multiple protein|gene pairs (default ":").
+
+    Returns
+    -------
+    tuple[str, str]
+        Tuple containing (protein_group, gene_group) as semicolon-separated strings.
+
+    Example
+    -------
+        >>> accession = "P1|G1:P2|G2:P3|G3"
+        >>> _assemble_pg_gene_groups(accession)
+        ("P1;P2;P3", "G1;G2;G3")
+
+    """
+    pg_gene_list = [
+        pair.split(id_separator) for pair in accession.split(group_separator)
+    ]
+
+    proteins = []
+    genes = []
+
+    for pair in pg_gene_list:
+        if len(pair) >= EXPECTED_PAIR_LENGTH:
+            proteins.append(pair[0])
+            genes.append(pair[1])
+        elif len(pair) == 1:
+            # If no gene provided, just add the protein
+            proteins.append(pair[0])
+
+    return ";".join(proteins), ";".join(genes)
 
 
 def _split_peaks_identifiers(df: pd.DataFrame) -> pd.DataFrame:
     """Split PEAKS protein group identifiers into separate protein and gene columns.
 
     The 'Accession' column contains both the protein and gene identifiers separated by
-    a pipe character. This method splits these identifiers into separate columns.
+    a pipe character. There may be multiple protein|gene pairs separated by a colon (??).
+    This function assembles all proteins into one protein group and all genes into a gene
+    group, separated by semicolons.
 
     Parameters
     ----------
     df
-        DataFrame containing PEAKS protein group data.
+        DataFrame containing PEAKS protein group report.
 
     Returns
     -------
     pd.DataFrame
-        DataFrame with separate protein and gene columns.
+        Modified copy of the input DataFrame with separate 'protein' and 'gene' columns.
 
     """
-    df[[PGCols.PROTEINS, PGCols.GENES]] = df[ACCESSION_COLUMN].str.split(
-        PROTEIN_GENE_SEPARATOR, expand=True
-    )
-    return df
+    df_copy = df.copy()
+
+    protein_groups = []
+    gene_groups = []
+
+    for accession in df_copy[ACCESSION_COLUMN]:
+        protein_group, gene_group = _assemble_pg_gene_groups(accession)
+        protein_groups.append(protein_group)
+        gene_groups.append(gene_group)
+
+    df_copy[PGCols.PROTEINS] = protein_groups
+    df_copy[PGCols.GENES] = gene_groups
+
+    return df_copy
 
 
 class PeaksPGReader(PGReaderBase):
