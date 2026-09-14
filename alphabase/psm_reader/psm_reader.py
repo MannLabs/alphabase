5b8a9f2109ee9d89b18acd40f7b05d9d90c2917d
Lucas Diedrich <diedrich@biochem.mpg.de>
2026-09-14 13:48:04 +0200

    [fix] Fix NaN handling in read_csv that coerces float columns with missing values to incorrect object dtype. Use default recognition of NaN values by pandas

--- alphabase/psm_reader/psm_reader.py
+++ alphabase/psm_reader/psm_reader.py
@@ -268,7 +268,7 @@ class PSMReaderBase(ABC):
         """
         if isinstance(filename, io.StringIO):
             sep = _get_delimiter(filename)
-            return pd.read_csv(filename, sep=sep, keep_default_na=False)
+            return pd.read_csv(filename, sep=sep, keep_default_na=True)
 
         file_path = Path(filename)
 
@@ -276,7 +276,7 @@ class PSMReaderBase(ABC):
             return pd.read_parquet(file_path)
 
         sep = _get_delimiter(str(file_path))
-        return pd.read_csv(file_path, sep=sep, keep_default_na=False)
+        return pd.read_csv(file_path, sep=sep, keep_default_na=True)
 
     def _get_actual_column(
         self,
